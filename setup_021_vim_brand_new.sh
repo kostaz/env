a318fd357739fafe49012a36c78be74e3210b83b
Kosta Zertsekel <zertsekel@gmail.com>
2015-07-26 15:13:27 -0400

    Add support for brand new dotvim repository

--- setup_021_vim_brand_new.sh
+++ setup_021_vim_brand_new.sh
@@ -3,4 +3,8 @@
 # Setup vim environment
 # ================================================================================
 rm -rf ~/.vim ~/.vimrc ~/.gvimrc && \
-cp -R .vim .vimrc .gvimrc .gitconfig ~
+git clone https://github.com/kostaz/dotvim ~/.vim && \
+cd ~/.vim && \
+git submodule init && \
+git submodule update && \
+ln -s ~/.vim/vimrc ~/.vimrc
