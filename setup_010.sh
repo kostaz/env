5b91894766af52094a5e2c7db4635ffe12d2abb8
Kosta Zertsekel <kzertsekel@mrv.com>
2015-08-16 08:59:46 +0300

    Add putty and krusader

--- setup_010.sh
+++ setup_010.sh
@@ -58,6 +58,8 @@ sudo apt-get install -y openjdk-8-jre && \
 sudo apt-get install -y tmux && \
 sudo apt-get install -y htop && \
 sudo apt-get install -y ranger && \
+sudo apt-get install -y putty && \
+sudo apt-get install -y krusader && \
 sudo dpkg --add-architecture i386 && \
 sudo apt-get update  -y && \
 sudo apt-get install -y lib32z1 && \
