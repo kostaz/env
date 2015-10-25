e99b18124f322fcb249ba06ed6ad824e970f7087
Kosta Zertsekel <zertsekel@gmail.com>
2015-10-25 19:13:04 -0400

    Add chrome and flash plugin

--- setup_040_gui.sh
+++ setup_040_gui.sh
@@ -5,4 +5,7 @@
 sudo apt-get install -y gnome-disk-utility && \
 sudo apt-get install -y gnome-calculator && \
 sudo apt-get install -y meld && \
-sudo apt-get install -y kdiff3
+sudo apt-get install -y kdiff3 && \
+sudo apt-get install -y chromium-browser && \
+sudo apt-get install -y flashplugin-installer && \
+sudo apt-get install -y adobe-flashplugin && \
