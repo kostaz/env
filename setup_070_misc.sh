27e5a91a7cb4eb7bdadec0ef7dd165508b6bb599
Kosta Zertsekel <zertsekel@gmail.com>
2015-12-20 16:21:02 +0200

    Add install script for misc stuff

---
+++ setup_070_misc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Setup needed programs and utilities
+# ================================================================================
+sudo apt-get install -y youtube-dl && \
+sudo apt-get install -y bison
