b3e10c626d544f982c99646611729fc955868913
Kosta Zertsekel <kzertsekel@mrv.com>
2016-01-25 13:32:06 +0200

    Add mics utils

--- setup_070_misc.sh
+++ setup_070_misc.sh
@@ -2,6 +2,9 @@
 
 # Setup needed programs and utilities
 # ================================================================================
+
+# http://ubuntuforums.org/showthread.php?t=2226702
+sudo apt-get install libmtp-common mtp-tools libmtp-dev libmtp-runtime libmtp9 && \
 sudo apt-get install -y ffmpeg && \
 sudo wget http://youtube-dl.org/latest/youtube-dl -O /usr/local/bin/youtube-dl && \
 sudo chmod a+rx /usr/local/bin/youtube-dl
