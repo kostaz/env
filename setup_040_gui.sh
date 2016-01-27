7c048a11fc2a487a938826dca9b6fac8df8e3544
Kosta Zertsekel <kzertsekel@mrv.com>
2016-01-27 10:40:26 +0200

    Add screen recorder Kazam

--- setup_040_gui.sh
+++ setup_040_gui.sh
@@ -6,6 +6,7 @@ sudo apt-get install -y gnome-disk-utility && \
 sudo apt-get install -y shutter && \
 sudo apt-get install -y gnome-calculator && \
 sudo apt-get install -y meld && \
+sudo apt-get install -y kazam && \
 sudo apt-get install -y kdiff3 && \
 sudo apt-get install -y chromium-browser && \
 sudo apt-get install -y flashplugin-installer && \
