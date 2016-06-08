025e076a61faf187dc64742dd2e6d20e6dafc1e3
Barak Adam <badam@mrv.com>
2016-06-08 15:49:03 +0300

    Add vino remote desktop

--- setup_070_misc.sh
+++ setup_070_misc.sh
@@ -4,7 +4,8 @@
 # ================================================================================
 
 # http://ubuntuforums.org/showthread.php?t=2226702
-sudo apt-get install libmtp-common mtp-tools libmtp-dev libmtp-runtime libmtp9 && \
+sudo apt-get install -y libmtp-common mtp-tools libmtp-dev libmtp-runtime libmtp9 && \
 sudo apt-get install -y ffmpeg && \
 sudo wget http://youtube-dl.org/latest/youtube-dl -O /usr/local/bin/youtube-dl && \
-sudo chmod a+rx /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl && \
+sudo apt-get install -y vino
