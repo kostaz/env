48f44fab2b5cfe78078edbcf2a99606a4a9d3ee5
Kosta Zertsekel <kzertsekel@mrv.com>
2016-06-16 16:44:46 +0300

    Add script to install various OPX Linux Kernel 4.4 stuff

---
+++ setup_090_opx_kernel_4_4.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Setup needed programs and utilities
+# ================================================================================
+sudo apt-get install -y ffmpeg && \
+sudo apt-get install -y virtualbox
