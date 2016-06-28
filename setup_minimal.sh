d5dd41f41f4703b2d494c1dcb339663921a4a854
Kosta Zertsekel <zertsekel@gmail.com>
2016-06-28 16:05:58 +0300

    Add minimal setup (vim, cscope, ctags, bashrc)

---
+++ setup_minimal.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Setup minimal environment
+# ================================================================================
+sudo apt-get install -y cscope
+sudo apt-get install -y exuberant-ctags
+./setup_011_bashrc.sh*
+./setup_020_vim.sh*
