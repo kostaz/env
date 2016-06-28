63de42dc1319cc7fc34802270df21c723343ca67
Kosta Zertsekel <zertsekel@gmail.com>
2016-06-28 16:14:27 +0300

    Update minimal setup (mc, byobu, minicom, axel, etc.)

--- setup_minimal.sh
+++ setup_minimal.sh
@@ -2,7 +2,17 @@
 
 # Setup minimal environment
 # ================================================================================
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install -y cscope
 sudo apt-get install -y exuberant-ctags
+sudo apt-get install -y mc
+sudo apt-get install -y byobu
+sudo apt-get install -y minicom
+sudo apt-get install -y dos2unix
+sudo apt-get install -y tree
+sudo apt-get install -y curl
+sudo apt-get install -y axel
+sudo apt-get install -y ncdu
 ./setup_011_bashrc.sh*
 ./setup_020_vim.sh*
