9500b11a2730f4a7310d9980eceaf93b737d6231
Kosta Zertsekel <kzertsekel@mrv.com>
2016-07-21 12:59:50 +0300

    Split vim and git stuff

--- setup_020_vim.sh
+++ setup_020_vim.sh
@@ -2,5 +2,8 @@
 
 # Setup vim environment
 # ================================================================================
+sudo apt-get install -y vim && \
+sudo apt-get install -y exuberant-ctags && \
+sudo apt-get install -y cscope && \
 rm -rf ~/.vim ~/.vimrc ~/.gvimrc && \
-cp -R .vim .vimrc .gvimrc .gitconfig ~
+cp -R .vim .vimrc .gvimrc ~
