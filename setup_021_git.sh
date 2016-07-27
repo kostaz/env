0bff7f1932d66e60603ea03b463345e89d422f66
Kosta Zertsekel <kzertsekel@mrv.com>
2016-07-27 17:24:33 +0300

    Add Git LFS install

--- setup_021_git.sh
+++ setup_021_git.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 cp -R .gitconfig ~
+
+# Git LFS
+# https://github.com/github/git-lfs/wiki/Installation
+sudo apt-get update
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+apt-get install git-lfs
+sudo apt-get install git-lfs
+git lfs install
