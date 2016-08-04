f039100dfb3b7517c21b81e72eab17258c348849
Kosta Zertsekel <kzertsekel@mrv.com>
2016-08-04 14:42:52 +0300

    Add git-email

--- setup_021_git.sh
+++ setup_021_git.sh
@@ -2,6 +2,10 @@
 
 cp -R .gitconfig ~
 
+# Install Git
+sudo apt-get install -y git
+sudo apt-get install -y git-email
+
 # Git LFS
 # https://github.com/github/git-lfs/wiki/Installation
 sudo apt-get update
@@ -9,6 +13,5 @@ sudo apt-get install python-software-properties
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt-get update
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-apt-get install git-lfs
-sudo apt-get install git-lfs
+sudo apt-get install -y git-lfs
 git lfs install
