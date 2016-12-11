a08192d48ef6804a0e855eebc21aec44dd663b00
Kosta Zertsekel <zertsekel@gmail.com>
2016-12-11 17:58:21 +0200

    Add zsh and oh-my-zsh install

---
+++ setup_120_zsh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Setup minimal environment
+# ==============================================================================
+sudo apt-get install -y zsh && \
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
