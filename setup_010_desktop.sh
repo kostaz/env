be01d506a7a8925efc8d22399406d34a14df1d42
Kosta Zertsekel <zertsekel@gmail.com>
2016-12-11 10:58:37 +0200

    Add terminator

--- setup_010_desktop.sh
+++ setup_010_desktop.sh
@@ -14,6 +14,7 @@ sleep 3
 sudo apt-get install -y git-cola && \
 sudo apt-get install -y tkcvs && \
 sudo apt-get install -y vim-gtk && \
+sudo apt-get install -y terminator && \
 sudo apt-get install -y emacs && \
 sudo apt-get install -y geany && \
 sudo apt-get install -y putty && \
