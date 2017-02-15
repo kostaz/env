c5e4d97693e1de0197af2a91ef459e3bb3c88fca
Kosta Zertsekel <kzertsekel@mrv.com>
2017-02-15 20:54:58 +0200

    Add fzf util (implemented in golang)
    Signed-off-by: Kosta Zertsekel <kzertsekel@mrv.com>

---
+++ setup_013_fzf_manual.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# A command-line fuzzy finder written in Go
+# https://github.com/junegunn/fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# Don't forget to install fzf.vim plugin:
+# https://github.com/junegunn/fzf.vim
