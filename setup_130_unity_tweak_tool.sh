4fd4fadc86ab765a622180f8db5a60f65312fa64
Kosta Zertsekel <kzertsekel@mrv.com>
2017-04-12 16:16:47 +0300

    Add script to install Unity Tweak Tool
    Signed-off-by: Kosta Zertsekel <kzertsekel@mrv.com>

---
+++ setup_130_unity_tweak_tool.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# To install Unity Tweak Tool
+# ================================================================================
+sudo apt-get install -y ffmpeg && \
+sudo apt-get install -y notify-osd && \
+sudo apt-get install -y unity-webapps-service && \
+sudo apt-get install -y unity-tweak-tool
