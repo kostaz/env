576ba6bdc51452449b444c6227bc23ccfeca83e2
Kosta Zertsekel <zertsekel@gmail.com>
2017-04-30 22:51:30 +0300

    Git: Remove Git LFS installs
    Signed-off-by: Kosta Zertsekel <zertsekel@gmail.com>

--- setup_021_git.sh
+++ setup_021_git.sh
@@ -10,8 +10,3 @@ sudo apt-get install -y git-email
 # https://github.com/github/git-lfs/wiki/Installation
 sudo apt-get update
 sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:git-core/ppa
-sudo apt-get update
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install -y git-lfs
-git lfs install
