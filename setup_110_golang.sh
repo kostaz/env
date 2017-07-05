33acb927057c228ebaf83316693843126b4bec1c
Kosta Zertsekel <kzertsekel@mrv.com>
2017-07-05 20:24:42 +0300

    Go: Add gocode and gotags
    Signed-off-by: Kosta Zertsekel <kzertsekel@mrv.com>

--- setup_110_golang.sh
+++ setup_110_golang.sh
@@ -5,3 +5,5 @@
 sudo add-apt-repository -y ppa:longsleep/golang-backports
 sudo apt-get update -y
 sudo apt-get install -y golang-go
+go get -u github.com/nsf/gocode
+go get -u github.com/jstemmer/gotags
