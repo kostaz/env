96ad9c52cabe6e335444967510ccdb16511080db
Kosta Zertsekel <zertsekel@gmail.com>
2017-07-16 11:31:21 +0300

    Add one script for server 16.04 machines
    Signed-off-by: Kosta Zertsekel <zertsekel@gmail.com>

---
+++ best_practice_all_server_16.04.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+# update /etc/sudoers
+# uncomment /etc/apt/sources.list
+# apt update
+# apt upgrade
+# reboot
+
+./setup_021_git.sh
+./setup_011_bashrc.sh
+./setup_010_server_16.04.sh
+./setup_110_golang.sh
+./setup_012_clang_silversearcher.sh
+./setup_020_vim_vundle.sh
+./setup_120_zsh.sh # has to be last
+
+# ./setup_013_fzf_manual.sh
+# update ~/.zshrc with source ~/.bashrc.kostaz
+# vim ~/.vimrc and PluginInstall
+# reboot
