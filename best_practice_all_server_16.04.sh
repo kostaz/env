e6c5a854906b844ea1613d7d98a915e98d60c701
Kosta Zertsekel <zertsekel@gmail.com>
2017-10-23 10:09:08 +0000

    Fix env installation sequence
    Signed-off-by: Kosta Zertsekel <zertsekel@gmail.com>

--- best_practice_all_server_16.04.sh
+++ best_practice_all_server_16.04.sh
@@ -6,20 +6,27 @@
 # apt upgrade
 # reboot
 
+# Batch 01
+# ========
 ./setup_021_git.sh
 ./setup_011_bashrc.sh
 ./setup_010_server_16.04.sh
 ./setup_030_server_manual.sh
 ./setup_150_smarts.sh
 ./setup_080_opx.sh
-./setup_110_golang.sh
 ./setup_012_clang_silversearcher.sh
-./setup_020_vim_vundle.sh
-./setup_120_zsh.sh # has to be last
 
-# ./setup_013_fzf_manual.sh
+# Batch 02
+# ========
+./setup_120_zsh.sh
 # update ~/.zshrc with:
 # 1) source ~/.bashrc.kostaz
 # 2) DISABLE_AUTO_TITLE="true" (to keep byobu tab name)
+
+# Batch 03
+# ========
+./setup_110_golang.sh
+./setup_020_vim_vundle.sh
+# ./setup_013_fzf_manual.sh
 # vim ~/.vimrc and PluginInstall
 # reboot
