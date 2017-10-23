1d16babfde8cec9f40dfd9a728ee0b8665d70143
Kosta Zertsekel <zertsekel@gmail.com>
2017-10-23 10:08:43 +0000

    Go: Remove golang-backports apt repo
    Signed-off-by: Kosta Zertsekel <zertsekel@gmail.com>

--- setup_110_golang.sh
+++ setup_110_golang.sh
@@ -2,8 +2,6 @@
 
 # https://github.com/golang/go/wiki/Ubuntu
 # ==============================================================================
-sudo add-apt-repository -y ppa:longsleep/golang-backports
-sudo apt-get update -y
 sudo apt-get install -y golang-go
 go get -u github.com/nsf/gocode
 go get -u github.com/jstemmer/gotags
