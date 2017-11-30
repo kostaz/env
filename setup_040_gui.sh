bf08fb5263c32a236965b1b9e767ecde999cced0
Kosta Zertsekel <kzertsekel@mrv.com>
2017-11-30 12:33:31 +0200

    GUI: Add Slack and Signal app installs
    Signed-off-by: Kosta Zertsekel <kzertsekel@mrv.com>

--- setup_040_gui.sh
+++ setup_040_gui.sh
@@ -15,10 +15,21 @@ sudo apt-get install -y chromium-browser && \
 sudo apt-get install -y filezilla
 
 sudo apt-get install -y flashplugin-installer
+sudo apt-get install -y deluge
 
 # Telegram
 sudo add-apt-repository -y ppa:atareao/telegram
 sudo apt-get update     -y
 sudo apt-get install    -y telegram
 
-sudo apt-get install    -y deluge
+# Signal
+curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
+echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+sudo apt update  -y
+sudo apt install -y signal-desktop
+
+# Slack
+sudo apt-add-repository -y ppa:rael-gc/scudcloud
+echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
+sudo apt update  -y
+sudo apt install -y scudcloud
