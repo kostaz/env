3623fa0a146eb5ae43b3f314365182b3a0ac66f1
Pavel Fridland <PFridland@advaoptical.com>
2018-02-28 13:13:24 +0200

    Fix: Install pip3 before ninja-build
    Signed-off-by: Pavel Fridland <PFridland@advaoptical.com>

--- setup_115_meson_build.sh
+++ setup_115_meson_build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo apt install -y python3 ninja-build
+sudo apt install -y python  python-pip
+sudo apt install -y python3 python3-pip
+
+sudo apt install -y ninja-build
 sudo apt install -y meson
 
 pip3 install --user meson
