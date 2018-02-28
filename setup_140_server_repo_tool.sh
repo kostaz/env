5242c7b006b445de8db41b90136609210633bbc1
Pavel Fridland <PFridland@advaoptical.com>
2018-02-28 13:12:57 +0200

    Fix: sudo curl ... > /bin/repo fails, split commands
    Signed-off-by: Pavel Fridland <PFridland@advaoptical.com>

--- setup_140_server_repo_tool.sh
+++ setup_140_server_repo_tool.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+sudo apt install -y
 
-curl https://storage.googleapis.com/git-repo-downloads/repo > /bin/repo
-chmod a+x /bin/repo
+# Take the timestamp.
+ts=`date +"%Y-%m-%d_%H:%M:%S"`
+
+tmp_file=/tmp/repo_tmp_$ts
+curl https://storage.googleapis.com/git-repo-downloads/repo > $tmp_file
+
+sudo cp $tmp_file /bin/repo
+sudo chmod a+x /bin/repo
