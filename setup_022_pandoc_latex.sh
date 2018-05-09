37f8645d65d1f823449f89c5d7055b1b13d3c67f
Kosta Zertsekel <zertsekel@gmail.com>
2018-05-09 17:49:32 +0300

    Latex: Add texlive and reference commands
    Signed-off-by: Kosta Zertsekel <zertsekel@gmail.com>

--- setup_022_pandoc_latex.sh
+++ setup_022_pandoc_latex.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+sudo apt-get install -y texlive && \
 sudo apt-get install -y pandoc && \
 sudo apt-get install -y texlive-latex-extra && \
 sudo apt-get install -y texlive-latex-recommended
+
+# Reference:
+# http://www.andy-roberts.net/writing/latex/absolute_beginners
+# Commands:
+#
+# $ touch hello.tex
+# $ gvim hello.tex
+# $ latex hello
+# $ dvipdf hello.dvi hello.pdf
+# $ evince hello.pdf
