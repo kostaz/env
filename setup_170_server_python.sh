51dcc6109103df6b6d97ee0eb68709352b348d66
Kosta Zertsekel <kzertsekel@advaoptical.com>
2018-06-21 17:02:13 +0300

    Add Python virtualenv (for buildbot)
    Signed-off-by: Kosta Zertsekel <kzertsekel@advaoptical.com>

--- setup_170_server_python.sh
+++ setup_170_server_python.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo apt-get install python-pip && \
-sudo apt-get install python3-pip && \
-pip install -U setuptools && \
-pip3 install -U setuptools && \
+sudo apt-get install -y virtualenv && \
+sudo apt-get install -y python-pip && \
+sudo apt-get install -y python3-pip && \
+sudo pip install -U setuptools && \
+sudo pip3 install -U setuptools && \
 sudo apt-get install -y python-setuptools &&\
 sudo pip install --upgrade youtube-dl
